3715577264eff8c9ef8934b65f77b805357ee3b8
Osmo Salomaa <otsaloma@iki.fi>
2019-06-02 02:12:36 +0300

    Add comment

--- dropbox-paper/dropbox-paper.js
+++ dropbox-paper/dropbox-paper.js
@@ -1,5 +1,8 @@
 // -*- coding: utf-8-unix -*-
 
+// Work around a file browser dialog always popping up.
+// https://github.com/jiahaog/nativefier/issues/737
+
 window.onload = function() {
     Dropbox.choose = undefined;
 };
